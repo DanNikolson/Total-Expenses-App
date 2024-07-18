403b331bea42a436cfe57cca1c3bf31c1ccc49a4
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-07-18 14:51:57 +0300

    Category relation ManyToOne to User entity, OneToMany to Transaction entity, transactions collection initialization in Category entity

--- app/Entity/Category.php
+++ app/Entity/Category.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\Table;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\OneToMany;
 
 #[Entity, Table('categories')]
 class Category
@@ -25,4 +29,15 @@ class Category
 
     #[Column(name: 'updated_at')]
     private DateTime $updatedAt;
+
+    #[ManyToOne(inversedBy: 'categories')]
+    private User $user;
+
+    #[OneToMany(mappedBy: 'category', targetEntity: Transaction::class)]
+    private Collection $transactions;
+
+    public function __construct()
+    {
+        $this->transactions = new ArrayCollection();
+    }
 }
