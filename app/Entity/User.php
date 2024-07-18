b81039414be4ba896d0e3f562a8bce6654a8f1a5
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-07-18 14:45:54 +0300

    User relation OneToMany to Category, Transaction; categories,transactions collections initialization in User entity

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\OneToMany;
 use Doctrine\ORM\Mapping\Table;
 
 #[Entity, Table('users')]
@@ -31,4 +34,17 @@ class User
 
     #[Column(name: 'updated_at')]
     private DateTime $updatedAt;
+
+    #[OneToMany(mappedBy: 'user', targetEntity: Category::class)]
+    private Collection $categories;
+
+    #[OneToMany(mappedBy: 'user', targetEntity: Transaction::class)]
+    private Collection $transactions;
+
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+        $this->transactions = new ArrayCollection();
+    }
 }
