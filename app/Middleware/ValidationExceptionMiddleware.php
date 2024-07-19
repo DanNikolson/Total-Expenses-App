e3e12f220ff22ca5842a107ba1a8e13a9136c9d1
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-07-19 14:47:23 +0300

    Added: ValidationExceptionMiddleware

---
+++ app/Middleware/ValidationExceptionMiddleware.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use App\Exception\ValidationException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+
+/**
+ * Middleware that handles ValidationExceptions and redirects to /register.
+ */
+class ValidationExceptionMiddleware implements MiddlewareInterface
+{
+    /**
+     * The factory used to create the redirect response.
+     *
+     * @var ResponseFactoryInterface
+     */
+    private ResponseFactoryInterface $responseFactory;
+
+    /**
+     * Create a new instance of ValidationExceptionMiddleware.
+     *
+     * @param ResponseFactoryInterface $responseFactory The factory used to create the redirect response.
+     */
+    public function __construct(ResponseFactoryInterface $responseFactory)
+    {
+        $this->responseFactory = $responseFactory;
+    }
+
+    /**
+     * Process the request and handle any ValidationExceptions that occur.
+     *
+     * If a ValidationException is thrown, a redirect to /register is returned.
+     *
+     * @param  ServerRequestInterface  $request The request object.
+     * @param  RequestHandlerInterface $handler The request handler.
+     * @return ResponseInterface       The response.
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+            return $handler->handle($request);
+        } catch (ValidationException $e) {
+            $response = $this->responseFactory->createResponse();
+
+            return $response->withHeader('Location', '/register')->withStatus(302);
+        }
+    }
+}
