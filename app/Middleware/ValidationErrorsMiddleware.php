cb92f02057127567d727be0386e07f42630398b9
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-07-19 20:23:26 +0300

    Updated: register view - displays now errors if input fields are empty, ValidationErrorsMiddleware - unsets errors, so they dissapear after page refresh

--- app/Middleware/ValidationErrorsMiddleware.php
+++ app/Middleware/ValidationErrorsMiddleware.php
@@ -31,7 +31,10 @@ class ValidationErrorsMiddleware implements MiddlewareInterface
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         if (!empty($_SESSION['errors'])) {
-            $this->twig->getEnvironment()->addGlobal('errors', $_SESSION['errors']);
+            $errors = $_SESSION['errors'];
+            $this->twig->getEnvironment()->addGlobal('errors', $errors);
+
+            unset($_SESSION['errors']);
         }
 
         return $handler->handle($request);
