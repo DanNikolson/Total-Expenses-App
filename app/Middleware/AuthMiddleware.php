350e6333c9207c72124bdcdd59a850a2a8eb3156
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-07-22 17:19:55 +0300

    Added: AuthMiddleware - to check if user is authenticated; Updated: web route - registered middleware for home and logout route

---
+++ app/Middleware/AuthMiddleware.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * Middleware that checks if a user is authenticated.
+ * If the user is not authenticated, it redirects to the login page.
+ */
+class AuthMiddleware implements MiddlewareInterface
+{
+    /**
+     * Create a new instance of AuthMiddleware.
+     *
+     * @param ResponseFactoryInterface $responseFactory The factory used to create the redirect response.
+     */
+    public function __construct(private readonly ResponseFactoryInterface $responseFactory)
+    {
+    }
+
+    /**
+     * Process the request and handle the authentication.
+     * If the user is not authenticated, it redirects to the login page.
+     *
+     * @param ServerRequestInterface $request The request object.
+     * @param RequestHandlerInterface $handler The request handler.
+     * @return ResponseInterface The response.
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if (empty($_SESSION['user'])) {
+            return $this->responseFactory->createResponse(302)->withHeader('Location', '/login');
+        }
+
+        return $handler->handle($request);
+    }
+}
--- configs/routes/web.php
+++ configs/routes/web.php
@@ -3,15 +3,16 @@
 declare(strict_types=1);
 
 use Slim\App;
+use App\Middleware\AuthMiddleware;
 use App\Controllers\AuthController;
 use App\Controllers\HomeController;
 
 return function (App $app) {
-    $app->get('/', [HomeController::class, 'index']);
+    $app->get('/', [HomeController::class, 'index'])->add(AuthMiddleware::class);
 
     $app->get('/login', [AuthController::class, 'loginView']);
     $app->get('/register', [AuthController::class, 'registerView']);
     $app->post('/login', [AuthController::class, 'logIn']);
     $app->post('/register', [AuthController::class, 'Register']);
-    $app->post('/logout', [AuthController::class, 'logOut']);
+    $app->post('/logout', [AuthController::class, 'logOut'])->add(AuthMiddleware::class);
 };
