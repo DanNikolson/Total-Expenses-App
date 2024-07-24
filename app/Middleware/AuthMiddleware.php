4691820a2c8a776f142f5beef92052afe7e3519a
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-07-24 15:29:43 +0300

    Updated: AuthMiddleware - combined logic with AuthenticateMiddleware, injected AuthInterface, middleware config - deleted AuthenticateMiddleware registration; Removed: AuthenticateMiddleware - logic moved to AuthMiddleware

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
-use Psr\Http\Message\ResponseFactoryInterface;
+use App\Contracts\AuthInterface;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
 
 /**
  * Middleware that checks if a user is authenticated.
@@ -21,13 +22,18 @@ class AuthMiddleware implements MiddlewareInterface
      *
      * @param ResponseFactoryInterface $responseFactory The factory used to create the redirect response.
      */
-    public function __construct(private readonly ResponseFactoryInterface $responseFactory)
-    {
+    public function __construct(
+        private readonly ResponseFactoryInterface $responseFactory,
+        private readonly AuthInterface $auth
+    ) {
     }
+
 
     /**
      * Process the request and handle the authentication.
-     * If the user is not authenticated, it redirects to the login page.
+     * If the user is authenticated, it will add the user to the request attributes and pass the request
+     * to the next middleware.
+     * If the user is not authenticated, it will create a redirect response to the login page.
      *
      * @param ServerRequestInterface $request The request object.
      * @param RequestHandlerInterface $handler The request handler.
@@ -35,10 +41,9 @@ class AuthMiddleware implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        if (empty($_SESSION['user'])) {
-            return $this->responseFactory->createResponse(302)->withHeader('Location', '/login');
+        if ($user = $this->auth->user()) {
+            return $handler->handle($request->withAttribute('user', $user));
         }
-
-        return $handler->handle($request);
+        return $this->responseFactory->createResponse(302)->withHeader('Location', '/login');
     }
 }
