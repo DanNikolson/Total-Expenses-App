73258cb59edaf420ddeeacbb364f29af7f00204b
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-07-25 17:22:05 +0300

    Updated: CsrfFieldsMiddleware - added(temporary) html csrf input fields to twig global environment; layout, login, register views - added csrf fields using twig global environment

--- app/Middleware/CsrfFieldsMiddleware.php
+++ app/Middleware/CsrfFieldsMiddleware.php
@@ -40,6 +40,10 @@ class CsrfFieldsMiddleware implements MiddlewareInterface
         $csrfValueKey = $csrf->getTokenValueKey();
         $csrfName     = $csrf->getTokenName();
         $csrfValue    = $csrf->getTokenValue();
+        $fields       = <<<CSRF_FIELDS
+            <input type="hidden" name="$csrfNameKey" value="$csrfName">
+            <input type="hidden" name="$csrfValueKey" value="$csrfValue">
+CSRF_FIELDS;
 
         $this->twig->getEnvironment()->addGlobal('csrf', [
             'keys' => [
@@ -47,7 +51,8 @@ class CsrfFieldsMiddleware implements MiddlewareInterface
                 'value' => $csrfValueKey
             ],
             'name'  => $csrfName,
-            'value' => $csrfValue
+            'value' => $csrfValue,
+            'fields' => $fields
         ]);
 
         return $handler->handle($request);
