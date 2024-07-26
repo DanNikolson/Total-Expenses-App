f0a5cfe8149fe77588f2d36f6140ae67b164ea46
Dan Nikolson <Kuznetsov556677@yandex.ru>
2024-07-26 18:57:09 +0300

    Updated: AuthMiddleware - process method adds user name and id to twig global env, layout view - displays logged in user's name

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
+use Slim\Views\Twig;
 use App\Contracts\AuthInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -24,9 +25,9 @@ class AuthMiddleware implements MiddlewareInterface
      */
     public function __construct(
         private readonly ResponseFactoryInterface $responseFactory,
-        private readonly AuthInterface $auth
-    ) {
-    }
+        private readonly AuthInterface $auth,
+        private readonly Twig $twig
+    ) {}
 
 
     /**
@@ -34,14 +35,20 @@ class AuthMiddleware implements MiddlewareInterface
      * If the user is authenticated, it will add the user to the request attributes and pass the request
      * to the next middleware.
      * If the user is not authenticated, it will create a redirect response to the login page.
+     * Adds user name and id to twig global, so it is possible to display it in views
      *
      * @param ServerRequestInterface $request The request object.
      * @param RequestHandlerInterface $handler The request handler.
      * @return ResponseInterface The response.
+     * @see App\Entity\User::getName()
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         if ($user = $this->auth->user()) {
+            /** @var UserInterface $user 
+             */
+            $this->twig->getEnvironment()->addGlobal('auth', ['id' => $user->getId(), 'name' => $user->getName()]);
+
             return $handler->handle($request->withAttribute('user', $user));
         }
         return $this->responseFactory->createResponse(302)->withHeader('Location', '/login');
